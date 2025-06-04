acf6eeffe42e88d6027bf209f53bbe5a9b0615ca
Remco Haszing <remcohaszing@gmail.com>
2025-06-04 16:15:44 +0200

    Update dev dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
-import config from '@remcohaszing/eslint'
+import { define } from '@remcohaszing/eslint'
 
-export default [...config, { ignores: ['fixtures'] }]
+export default define([{ ignores: ['fixtures'] }])
